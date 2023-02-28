6b0890ee52d934f1f249b7af64f067e8f4df3f7a
krisna <krisnamadani@gmail.com>
2023-02-28 19:42:07 +0700

    fix route product list from admin

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::get('/toko', [App\Http\Controllers\API\UserController::class, 'toko']);
 Route::get('/produk/{id}', [App\Http\Controllers\API\UserController::class, 'detail_produk']);
 Route::get('/toko/{id}', [App\Http\Controllers\API\UserController::class, 'detail_toko']);
 
-Route::get('/toko/{id}/produk'. [App\Http\Controllers\API\UserController::class, 'produk_toko']);
+Route::get('/toko/{id}/produk', [App\Http\Controllers\API\UserController::class, 'produk_toko']);
 
 Route::post('/login', [App\Http\Controllers\API\AdminController::class, 'login']);
 Route::post('/register', [App\Http\Controllers\API\AdminController::class, 'register']);
